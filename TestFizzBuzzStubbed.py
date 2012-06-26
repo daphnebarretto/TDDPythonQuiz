908b066af42ebd22e36f2f02e195fa4457b8dbf2
daphnebarretto <daphnebarretto@gmail.com>
2012-06-26 18:35:12 +0530

    Update master

--- TestFizzBuzzStubbed.py
+++ TestFizzBuzzStubbed.py
@@ -3,30 +3,34 @@ import pymock
 import FizzBuzz
 """
 Q3. What will be printed when we execute 'python FizzBuzzStubbed.py' ? [3 pts]
-
- assumimg it was python TestFizzBuzzStubbed.py :)
-    setUpClass FizzBuzzStubbed
-        setup
-        test_report
-        teardown
-        setup
-        test_report
-        teardown
-    tearDownClass
+setUpClass FizzBuzzStubbed
+setup
+test_report
+teardown
+setup
+test_report
+teardown
+tearDownClass
 
 Q4. Implement MyStub class so that you can send it as a fake object to the
     report method of FizzBuzz object from a test case. [3 pts]
 
 """
 class MyStub(object):
-    pass
+ 
+    def __init__(self):
+        self.values = []
+		
+	def write(self, value):
+        self.values.append(value)
+	    
+    def close(self):
+        self.closed = True
 
-
-
-
-
-
-
+    def gen_open_stub(self):
+        def open(fpath, mode):
+            return self
+        return open    
 
     
 class TestFizzBuzzStubbed(unittest.TestCase):
@@ -51,11 +55,18 @@ class TestFizzBuzzStubbed(unittest.TestCase):
 
     def test_report(self):
         print "test_report"
-        pass
+        mystub = MyStub()
+      		numbers =[1,2,3,4]
+      		fileOpener = mystub.gen_open_stub()		
+      		report(numbers, fileOpener)
+      		self.assertEqual(values[2], '3 fizz \n')	
 
     def test_report_for_empty_list(self):
         print "test_report"
-        pass
+        mystub = MyStub()
+ 	      numbers =[]
+		      fileOpener = mystub.gen_open_stub()		
+		      report(numbers, fileOpener)
 
 if __name__ == "__main__":
     unittest.main()
